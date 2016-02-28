52a060e2da6fa17a75ea566b3921b4bbefbcc6e9
ReedRichard <RichardReed@Outlook.com>
2016-02-28 00:21:27 -0800

    less shitty background

---
+++ Supreme Octupus Rotory Phone/Assets/WavyBackground.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WavyBackground : MonoBehaviour {
+	public Vector2 Speed;
+
+	private Renderer quadRenderer;
+
+	public bool hasStarted;
+
+	// Use this for initialization
+	void Start () {
+		quadRenderer = this.gameObject.GetComponent<Renderer> ();
+	}
+
+	void LateUpdate() {
+		if(hasStarted) quadRenderer.material.mainTextureOffset = Speed * Time.time;
+	}
+
+	public void StartScreenMovement() {
+		hasStarted = true;
+	}
+
+	public void PauseScreenMovement() {
+		hasStarted = false;
+	}
+}
