b811420bc0c9f202490a238fe8cde2b12f3dae64
Ansonious <jake.s.leach@gmail.com>
2016-02-28 18:46:30 -0800

    Combos and end game score screen
    Combos and end game score screen

---
+++ Supreme Octupus Rotory Phone/Assets/Scripts/GameOverScore.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class GameOverScore : MonoBehaviour {
+
+	public Text finalScore;
+
+	// Use this for initialization
+	void Start () {
+		finalScore.text = "Final score: " + PlayerPrefs.GetInt ("Current Score");
+	}
+
+}
--- Supreme Octupus Rotory Phone/Assets/Scripts/ScoreController.cs
+++ Supreme Octupus Rotory Phone/Assets/Scripts/ScoreController.cs
@@ -5,11 +5,15 @@ using System.Collections;
 public class ScoreController : MonoBehaviour {
 
 	public Text scoreText;
+
 	private int score = 0;
+	private int combo = 0;
+	private int multiplier = 0;
 
 	// Use this for initialization
 	void Awake() {
 		Phone.onPhoneClick += this.updateScore;
+		Phone.onPhoneDeath += this.resetCombo;
 	}
 
 	void Start() {
@@ -17,8 +21,17 @@ public class ScoreController : MonoBehaviour {
 	}
 
 	void updateScore() {
-		score++;
+		combo++;
+		if (combo < 110) {
+			multiplier = (combo / 10) + 1;
+		}
+		score += multiplier;
 		scoreText.text = "Score: " + score;
 		PlayerPrefs.SetInt ("Current Score", score);
+	}
+
+	void resetCombo() {
+		combo = 0;
+		multiplier = 0;
 	}
 }
