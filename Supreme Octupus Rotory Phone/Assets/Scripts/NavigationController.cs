4b34b87e2b34741b1132c20ba53974a8be8a47ca
imahassle <imahassle@gmail.com>
2016-02-28 16:54:20 -0800

    Nav changes

---
+++ Supreme Octupus Rotory Phone/Assets/Scripts/NavigationController.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+using System.Collections.Generic;
+
+public class NavigationController : MonoBehaviour {
+
+	public enum sceneEnum {
+		StartMenu,
+		HighScore,
+		Level
+	};
+
+	string[] sceneVals = {
+		"StartMenu",
+		"HighScore",
+		"Level"
+	};
+
+	public sceneEnum input;
+
+	private ArrayList scenes;
+
+	public void Start () {
+	}
+
+	public void NavigateToScene() {
+		SceneManager.LoadScene (input.ToString());
+	}
+}
